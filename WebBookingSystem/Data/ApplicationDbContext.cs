5092c099561ef9cead2a5101ecf741280896368f
cuongngodev <2238147@johnabbottcollege.net>
2025-11-07 10:39:23 -0500

    Add seeder service.

--- WebBookingSystem/Data/ApplicationDbContext.cs
+++ WebBookingSystem/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookingSystem.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebBookingSystem.Data
@@ -9,5 +10,7 @@ namespace WebBookingSystem.Data
             : base(options)
         {
         }
+        public DbSet<Service> Services { get; set; }
+
     }
 }
