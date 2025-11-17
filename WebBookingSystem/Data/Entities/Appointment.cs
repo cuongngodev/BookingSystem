c1367025a6e4cd401219a2ca0da7920ebb1d155f
6197673 <6197673@johnabbottcollege.net>
2025-11-17 12:47:59 -0500

    feat: Create appointment entity

---
+++ WebBookingSystem/Data/Entities/Appointment.cs
@@ -0,0 +1,21 @@
+﻿namespace WebBookingSystem.Data.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ServiceId { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public AppointmentStatus Status { get; set; } = AppointmentStatus.Pending;
+        public string? Notes { get; set; }
+
+    }
+
+    public enum AppointmentStatus
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Completed = 2,
+        Cancelled = 3,
+    }
+}
