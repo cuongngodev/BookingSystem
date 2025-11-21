d5026c3110cb5864cf51e5db04502072cea1c6ce
Cuong Ngo <133584912+cuongngodev@users.noreply.github.com>
2025-11-21 16:56:32 -0500

    Create AuthController

--- WebBookingSystem/Controllers/AuthController.cs
+++ WebBookingSystem/Controllers/AuthController.cs
@@ -1,1 +1,121 @@
-﻿
+﻿using BookingSystem.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebBookingSystem.Data;
+using WebBookingSystem.Models;
+
+namespace WebBookingSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole<int>> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        private readonly ApplicationDbContext _context;
+
+
+        public AuthController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole<int>> roleManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext _context
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+            var userExists = await _userManager.FindByEmailAsync(registerVM.Email);
+            // check user exist
+            if(userExists != null)
+            {
+                ModelState.AddModelError("", "User already exists");
+                return View(registerVM);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerVM.Email,
+                FirstName = registerVM.FirstName,
+                LastName = registerVM.LastName,
+                UserName = GenerateUsernameFromEmail(registerVM.Email),
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+            // create user
+            var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+                Console.WriteLine($"Error: {error.Code} - {error.Description}");
+
+            }
+
+            return View(registerVM);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.Email);
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, loginVM.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(loginVM);
+        }
+        public static string GenerateUsernameFromEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return string.Empty;
+
+            int atIndex = email.IndexOf('@');
+            if (atIndex > 0)
+            {
+                return email.Substring(0, atIndex);
+            }
+
+            return email;
+        }
+    }
+}
