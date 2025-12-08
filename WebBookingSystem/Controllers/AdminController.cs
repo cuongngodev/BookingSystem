f229861701baa54feec40dadce19e5551bcbfd37
Cuong Ngo <nvquoccuong1511@gmail.com>
2025-12-08 01:54:59 -0500

    add admin controller

---
+++ WebBookingSystem/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBookingSystem.Data.Intefaces;
+
+namespace WebBookingSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<AppointmentsController> _logger;
+        public AdminController(IUnitOfWork unitOfWork, ILogger<AppointmentsController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
